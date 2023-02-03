88ef5569ee27fbb8bd62125709468359f0f10222
Tamara <tamarag.dev@gmail.com>
2023-02-03 08:18:50 +0100

    test de la fonction

--- Kata BigBangBoom/BigBangFunction.ts
+++ Kata BigBangBoom/BigBangFunction.ts
@@ -1,3 +1,19 @@
 function BigBang(number: number) : string {
-   return 'bonjour'
+   var result: string = ""
+
+   return result
 }
+
+console.log(BigBang(1) === '1'? true : false) 
+console.log(BigBang(2) === '2'? true : false)
+console.log(BigBang(5) === 'Big'? true : false)
+console.log(BigBang(7) === 'Bang'? true : false)
+console.log(BigBang(11) === 'Boom'? true : false)
+console.log(BigBang(22) === 'Boom'? true : false)
+console.log(BigBang(35) === 'BigBang'? true : false)
+console.log(BigBang(70) === 'BangBig'? true : false)
+console.log(BigBang(55) === 'BigBoom'? true : false)
+console.log(BigBang(110) === 'BoomBig'? true : false)
+console.log(BigBang(77) === 'BangBoom'? true : false)
+console.log(BigBang(385) === 'BigBangBoom'? true : false)
+console.log(BigBang(770) === 'BoomBangBig'? true : false)
